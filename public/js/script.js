dd7e345fe1dbbbed7d5ac95ac384ac1b40490838
Kiril_Velichkov <kvelichkow@gmail.com>
2016-12-18 12:00:04 +0200

    completed courses main page added styling to navigation when clicked

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,20 @@
 function showCourses() {
-    divToShow = document.getElementById('courses-nav-container');
-    divToHide = document.getElementById('content');
-    divToShow.style.display = "block";
-    divToHide.style.display = "none";
+
+    var $divToShow = $('#courses-nav-container');
+    var $divToHide = $('#content');
+    
+    if ($divToShow.is(':visible')){
+        $divToHide.show();
+        $divToShow.hide();
+         $('#courses').removeClass('menu-active');
+    }else{
+        $divToShow.show();
+        $divToHide.hide();
+         $('#courses').addClass('menu-active');
+    }
+
+    // divToShow = document.getElementById('courses-nav-container');
+    // divToHide = document.getElementById('content');
+    // divToShow.style.display = 'block';
+    // divToHide.style.display = 'none';
 }
